2b9277d5adc0a67b0fcaff9b98ef5d3924635d74
Denzel Tovar <denzeltov@fb.com>
2020-06-25 17:00:49 -0700

    Add remaining Files

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by admin on 6/24/20.
+//  Copyright © 2020 Denzel Tovar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flix/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Flix
+//
+//  Created by admin on 6/24/20.
+//  Copyright © 2020 Denzel Tovar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
